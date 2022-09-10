1cc55d4db666d86b782d0d5eb4df71f39ba06dcb
simone-alcala <simone.alcala@gmail.com>
2022-09-10 10:38:08 -0300

    Fix: cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,8 +6,8 @@ import router from './routers/index';
 import errorHandler from './middlewares/errorHandlerMiddleware';
 
 const app = express();
-app.use(json());
 app.use(cors());
+app.use(json());
 app.use(router);
 app.use(errorHandler);
 
