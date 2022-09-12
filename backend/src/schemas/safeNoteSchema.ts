525e54f36841bb0c78c22cddc40c46d28beb8a04
simone-alcala <simone.alcala@gmail.com>
2022-09-12 17:15:20 -0300

    Fix: SafeNotes schema

--- backend/src/schemas/safeNoteSchema.ts
+++ backend/src/schemas/safeNoteSchema.ts
@@ -3,6 +3,6 @@ import Joi from 'joi';
 import { RequestSafeNoteData } from '../types/safeNoteTypes';
 
 export const safeNote = Joi.object<RequestSafeNoteData>({
-  title: Joi.string().trim().required(),
-  note: Joi.string().trim().required()
+  title: Joi.string().trim().max(50).required(),
+  note: Joi.string().trim().max(1000).required()
 });
